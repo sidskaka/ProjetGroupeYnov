6916e378ef19279125cda14c2d55f05b1dd2eac7
sidskaka <sidyka01@gmail.com>
2020-11-26 15:22:48 +0100

    commit sids

--- src/components/Logout/index.js
+++ src/components/Logout/index.js
@@ -1,11 +1,14 @@
-/**import React, { useEffect } from 'react'
+import React, { useEffect, useContext } from 'react'
 import { useHistory } from 'react-router-dom'
+import { FirebaseContext } from '../Firebase'
 
 const Logout = () => {
     const history = useHistory()
+    const firebase = useContext(FirebaseContext)
 
-    useEffect(() => {
-        localStorage
+    useEffect(() => {        
+        firebase.signoutCustomer();
+        localStorage.clear();
         history.push('/')
     })
     return (
@@ -15,12 +18,13 @@ const Logout = () => {
     )
 }
 
-export default Logout*/
-import { FirebaseContext } from '../Firebase'
+export default Logout
+/**import { FirebaseContext } from '../Firebase'
 import { useContext } from 'react'
 import { useHistory } from 'react-router-dom'
 
 class Logout {
+    
     firebase = useContext(FirebaseContext)
     history = useHistory()
     setLogout() {
@@ -29,4 +33,4 @@ class Logout {
     }
 }
 
-export default Logout
+export default Logout*/
